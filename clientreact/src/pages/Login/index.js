8c870326c591c4bc03155a00946da475003a615f
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-05-05 11:12:52 -0300

    Colocando a imagem na pasta assets

--- clientreact/src/pages/Login/index.js
+++ clientreact/src/pages/Login/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './styles.css';
-//import logoImage from '../../assets/login.png';
+import logoImage from '../../assets/login.png';
 
 export default function Login(){
     return(
     <div className="login-container">
         <section className="form">
-            {/* <img src={logoImage} alt="Login" id="img1"/> */}
+            { <img src={logoImage} alt="Login" id="img1"/>}
             <form>
                 <h1>Cadastro de Alunos</h1>
                 <input placeholder="Email"/>
