492dd6f6d2b20ef3828d265ed8c813c8c675d091
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2023-05-10 20:28:23 -0300

    Finalização da Aula 14

--- clientreact/src/pages/NovoAluno/index.js
+++ clientreact/src/pages/NovoAluno/index.js
@@ -1,18 +1,27 @@
 import React from "react";
 import './styles.css';
+import { FiCornerDownLeft, FiUserPlus } from 'react-icons/fi';
+import { Link, useParams } from "react-router-dom";
 
 export default function NovoAluno() {
+    const { alunoId } = useParams();
+
     return (
         <div className="novo-aluno-container">
             <div className="content">
-                <h1>Texto</h1>
                 <section className="form">
+                    <FiUserPlus size="105" color="#17202a" />
+                    <h1>{alunoId === '0' ? 'Incluir Novo Aluno' : 'Atualizar Aluno'}</h1>
+                    <Link className="back-link" to="/alunos">
+                        <FiCornerDownLeft size="25" color="#17202a" />
+                        Retornar
+                    </Link>
                 </section>
                 <form >
                     <input placeholder="Nome" />
                     <input placeholder="Email" />
                     <input placeholder="Idade" />
-                    <button className="button" type="submit">Texto</button>
+                    <button className="button" type="submit">{alunoId === '0' ? 'Incluir' : 'Atualizar'}</button>
                 </form>
             </div>
         </div>
